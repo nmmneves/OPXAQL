7cea3e00e403c1364e987cb8f266291e36b8b171
nmmneves <nmm.neves@campus.fct.unl.pt>
2018-07-03 18:20:50 +0100

    Update start.sh

--- StartControllerScript/start.sh
+++ StartControllerScript/start.sh
@@ -1,8 +1,13 @@
+#This exported variables are used to configure the IP of both Antidote and AQL nodes
+#so that the controller is able to connect himself and the switches via script
 export PB_IP=$(/sbin/ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1) 
 export IP=$PB_IP
 export ANTIDOTE_NODENAME2=antidote@$IP
 NODES=$ANTIDOTE_NODENAME2
-#export AQL_NAME=aql@$(/sbin/ifconfig |grep -A 1 eth0 | tail -l |cut -d : -f 2 | cut -d -f l))
+#Cleans Antidote logs and previous data, and start a new empty Antidote shell.
+#The make compile and release step are not always necessary, but are important
+#when the script is executed for the first time. 
+#Screen is used to keep the shell active in the background
 echo "Starting Antidote"
 cd antidote
 rm -rf data.antidote@*
@@ -14,12 +19,17 @@ rm -rf _build/default/rel/
 make compile > /dev/null 
 make rel > /dev/null
 screen -S antidote -d -m bash -c '_build/default/rel/antidote/bin/env console;exec sh' 
-#_build/default/rel/antidote/bin/env console
+#Starts AQL. 
+#Screen is used again to keep the shell active in the background.
 echo "Starting AQL"
 cd ..
 cd AQL
 screen -S AQL -d -m bash -c 'make shell;exec sh'
-#make shell
+#The sleep is used to make sure that the AQL instance is
+#up and running when the script that joins every component 
+#is executed. This script requires as argument every node
+#that we want to connect. Thats another reason why the machine's
+#IP is necessary
 sleep 4
 echo "Connecting Controller to Switch with IP"
 echo $PB_IP
@@ -29,8 +39,10 @@ echo $ip
 NODES+=" antidote@"$ip
 done
 ./join_dcs_script.erl $NODES 
+#Create the tables necessary for the controller to function.
+#Then ends by starting the controller processes
 echo "Starting Table Creation"
 cd ..
 sudo python createtablescontroller.py
-echo Starting Controller Initialization
+echo "Starting Controller Initialization"
 sudo python -m ControllerDBCP.EventHandler
