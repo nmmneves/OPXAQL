4015cef89691755febc1d4fe332d7e794178fae9
nmmneves <nmm.neves@campus.fct.unl.pt>
2018-07-10 12:44:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,13 @@ sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get install screen
 git clone https://github.com/pedromslopes/antidote.git
 cd antidote
 git checkout aql_oriented_features
+#git checkout 25aaa7a5c306ad5ba534c5a6281794922fe6c6a4
 git config --global url.https://github.com/.insteadOf git://github.com/
 cd ..
 git clone https://github.com/pedromslopes/AQL.git
 cd AQL
 git checkout new_features
+#git checkout eb68cfa20c89187e2b26a0d47b99e46be0b978e9
 cd ..
 sudo rm -rf otp_src_19.3.tar.gz
 sudo rm -rf otp_src_19.3
@@ -43,6 +45,7 @@ then
  sudo rm -rf StartSwitchScript
  mv StartControllerScript/start.sh .
  rm -rf StartControllerScript
+ chmod +x join_dcs_script.erl
 else
  echo "Choose between Switch or Controller"
 fi
