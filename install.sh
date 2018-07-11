e44a88623df4ad02a20c5065c287bace9d5a8c33
nmmneves <nmm.neves@campus.fct.unl.pt>
2018-07-11 16:42:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+if [ $# -ne 1]; then
+ echo "Invalid number of arguments. The number of arguments must be exactly 1"
+ exit
+fi
+if [ $1 != "Switch" ] && [ $1  != "Controller" ]; then
+ echo "Invalid name. Choose between Switch or Controller"
+ exit
+fi
 sudo apt-get -y install build-essential && sudo apt-get -y update && sudo apt-get -y upgrade
 wget erlang.org/download/otp_src_19.3.tar.gz
 tar zfx otp_src_19.3.tar.gz
@@ -10,14 +18,20 @@ cd ..
 sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get install screen && sudo apt-get install curl
 git clone https://github.com/pedromslopes/antidote.git
 cd antidote
-git checkout aql_oriented_features
-#git checkout 25aaa7a5c306ad5ba534c5a6281794922fe6c6a4
+if [ $1 = "Switch" ]; then
+ git checkout aql_oriented_features
+else
+ git checkout 25aaa7a5c306ad5ba534c5a6281794922fe6c6a4
+fi
 git config --global url.https://github.com/.insteadOf git://github.com/
 cd ..
 git clone https://github.com/pedromslopes/AQL.git
 cd AQL
-git checkout new_features
-#git checkout eb68cfa20c89187e2b26a0d47b99e46be0b978e9
+if [ $1 = "Switch" ]; then
+ git checkout new_features
+else
+ git checkout eb68cfa20c89187e2b26a0d47b99e46be0b978e9
+fi
 cd ..
 sudo rm -rf otp_src_19.3.tar.gz
 sudo rm -rf otp_src_19.3
@@ -30,16 +44,14 @@ mv changetoinstall/aql_http_handler.erl AQL/src/
 rm -rf changetoinstall
 rm -rf OPXAQL
 rm README.md
-if [ $1 = "Switch" ]
-then
+if [ $1 = "Switch" ]; then
  sudo rm -rf ControllerDBCP
  rm createtablescontroller.py
  sudo rm -rf StartControllerScript
  mv StartSwitchScript/start.sh .
  rm -rf StartSwitchScript
  rm join_dcs_script.erl
-elif [ $1 = "Controller" ]
-then
+else
  sudo rm -rf SwitchDBCP
  rm createtablesswitch.py
  sudo rm -rf StartSwitchScript
@@ -47,8 +59,6 @@ then
  rm -rf StartControllerScript
  rm interfacestatechange.py
  chmod +x join_dcs_script.erl
-else
- echo "Choose between Switch or Controller"
 fi
 chmod +x start.sh
 chmod +x stop.sh
